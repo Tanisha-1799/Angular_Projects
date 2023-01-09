7d69b65ad7b99780b28b2dfffa7039927a25c8eb
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-09 18:08:24 +0530

    Designing the question page

--- AngularQuizApp/src/app/question/question.component.ts
+++ AngularQuizApp/src/app/question/question.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
+  public name : string="";
 
   constructor() { }
 
   ngOnInit(): void {
+    //! mark is added to make sure that initially the name should have a value 
+    //it should not be undefined because if it is undefined it will throw error
+    
+    this.name=localStorage.getItem("name")!;
   }
 
 }
--- AngularQuizApp/src/app/welcome/welcome.component.ts
+++ AngularQuizApp/src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  //We are using viewChild to store the name entered by the user
+  @ViewChild('name') namekey!:ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  //This is a function that would run whenever the start button is clicked
+  //purpose :we will store the name entered and display it on the next pages.
+  startQuiz(){
+    localStorage.setItem("name",this.namekey.nativeElement.value);
   }
 
 }
