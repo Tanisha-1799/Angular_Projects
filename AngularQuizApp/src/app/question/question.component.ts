7bd4de18a6988f5fc14fe2e1d1b292350bbbdd58
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-09 18:31:17 +0530

    calling service to the question page

--- AngularQuizApp/src/app/question/question.component.ts
+++ AngularQuizApp/src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../service/question.service';
 
 @Component({
   selector: 'app-question',
@@ -7,14 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
   public name : string="";
-
-  constructor() { }
+  //constructor is used for injecting the services 
+  //so we first assign a variable and then import the service
+  constructor(private questionService : QuestionService) { }
 
   ngOnInit(): void {
     //! mark is added to make sure that initially the name should have a value 
     //it should not be undefined because if it is undefined it will throw error
-    
+
     this.name=localStorage.getItem("name")!;
+    this.getAllQuestions();
+  }
+  //calling the service to get the questions
+
+  getAllQuestions(){
+    this.questionService.getQuestionJson()
+    .subscribe((res: any)=>{
+      console.log(res);
+    })
+
   }
 
 }
--- AngularQuizApp/src/app/service/question.service.ts
+++ AngularQuizApp/src/app/service/question.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+//now we have to import httpClient module in app.modules
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getQuestionJson(){
+    //to get the json
+    return this.http.get<any>("assets/questions.json");
+  }
 }
